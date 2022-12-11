d4a9da34f7af4e123808e012d3cca8f19b2a2bf4
Mostafa Moradian <mostafamoradian0@gmail.com>
2022-12-11 23:48:18 +0100

    Include handshake config directly in the server config

--- main.go
+++ main.go
@@ -7,7 +7,11 @@ import (
 
 func main() {
 	goplugin.Serve(&goplugin.ServeConfig{
-		HandshakeConfig: plugin.Handshake,
+		HandshakeConfig: goplugin.HandshakeConfig{
+			ProtocolVersion:  1,
+			MagicCookieKey:   "GATEWAYD_PLUGIN",
+			MagicCookieValue: "5712b87aa5d7e9f9e9ab643e6603181c5b796015cb1c09d6f5ada882bf2a1872",
+		},
 		Plugins: goplugin.PluginSet{
 			"gateway-plugin-test": plugin.NewTestPlugin(plugin.Plugin{}),
 		},
