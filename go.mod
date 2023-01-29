698c344393701402da3225a88123236778f68299
Mostafa Moradian <mostafa@gatewayd.io>
2023-01-29 16:15:00 +0100

    Use a logger to send plugin logs to the GatewayD

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/gatewayd-io/gatewayd-plugin-test
 go 1.18
 
 require (
+	github.com/hashicorp/go-hclog v1.4.0
 	github.com/hashicorp/go-plugin v1.4.8
 	google.golang.org/grpc v1.51.0
 	google.golang.org/protobuf v1.28.1
@@ -11,7 +12,6 @@ require (
 require (
 	github.com/fatih/color v1.13.0 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
-	github.com/hashicorp/go-hclog v1.4.0 // indirect
 	github.com/hashicorp/yamux v0.1.1 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.16 // indirect
--- main.go
+++ main.go
@@ -1,11 +1,23 @@
 package main
 
 import (
+	"os"
+
 	"github.com/gatewayd-io/gatewayd-plugin-test/plugin"
+	"github.com/hashicorp/go-hclog"
 	goplugin "github.com/hashicorp/go-plugin"
 )
 
 func main() {
+	logger := hclog.New(&hclog.LoggerOptions{
+		Level:      hclog.Debug,
+		Output:     os.Stderr,
+		JSONFormat: true,
+		Color:      hclog.ColorOff,
+	})
+	pluginInstance := plugin.NewTestPlugin(plugin.Plugin{
+		Logger: logger,
+	})
 	goplugin.Serve(&goplugin.ServeConfig{
 		HandshakeConfig: goplugin.HandshakeConfig{
 			ProtocolVersion:  1,
@@ -13,8 +25,9 @@ func main() {
 			MagicCookieValue: "5712b87aa5d7e9f9e9ab643e6603181c5b796015cb1c09d6f5ada882bf2a1872",
 		},
 		Plugins: goplugin.PluginSet{
-			"gateway-plugin-test": plugin.NewTestPlugin(plugin.Plugin{}),
+			"gateway-plugin-test": pluginInstance,
 		},
 		GRPCServer: goplugin.DefaultGRPCServer,
+		Logger:     logger,
 	})
 }
