d1144976b306410fa96151a65b255ceed2663acc
Mostafa Moradian <mostafa@gatewayd.io>
2023-01-29 16:15:35 +0100

    Start a metrics HTTP server on a unix socket

--- main.go
+++ main.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"flag"
+	"net"
+	"net/http"
 	"os"
 
 	"github.com/gatewayd-io/gatewayd-plugin-test/plugin"
 	"github.com/hashicorp/go-hclog"
 	goplugin "github.com/hashicorp/go-plugin"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 func main() {
@@ -20,9 +23,54 @@ func main() {
 		JSONFormat: true,
 		Color:      hclog.ColorOff,
 	})
+
 	pluginInstance := plugin.NewTestPlugin(plugin.Plugin{
 		Logger: logger,
 	})
+
+	go func() {
+		config := map[string]string{}
+		for key, value := range plugin.Config["config"].(map[string]interface{}) {
+			if value, ok := value.(string); ok {
+				config[key] = value
+			}
+		}
+
+		if _, ok := config["metricsPath"]; !ok {
+			logger.Error("Failed to start metrics server", "error", "metricsPath not set")
+		}
+
+		if _, ok := config["socketPath"]; !ok {
+			logger.Error("Failed to start metrics server", "error", "socketPath not set")
+		}
+
+		if file, err := os.Stat(config["socketPath"]); err == nil {
+			if file.Mode().Type() == os.ModeSocket {
+				if err := os.Remove(config["socketPath"]); err != nil {
+					logger.Error("Failed to remove existing socket", "error", err)
+					return
+				}
+			} else {
+				logger.Error("Failed to remove existing socket", "error", "file is not a socket")
+				return
+			}
+		}
+
+		listener, err := net.Listen("unix", config["socketPath"])
+		if err != nil {
+			logger.Error("Failed to create a listener for metrics server", "error", err)
+		}
+
+		logger.Info(
+			"Starting metrics server",
+			"socketPath", config["socketPath"],
+			"metricsPath", config["metricsPath"])
+
+		if err := http.Serve(listener, promhttp.Handler()); err != nil {
+			logger.Error("Failed to start metrics server")
+		}
+	}()
+
 	goplugin.Serve(&goplugin.ServeConfig{
 		HandshakeConfig: goplugin.HandshakeConfig{
 			ProtocolVersion:  1,
